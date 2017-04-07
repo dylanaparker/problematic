71d2cc2bd32bc07c1669e3818b0d156d8270bfdd
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-07 03:52:00 -0700

    completed problem 2

---
+++ grettings.rb
@@ -0,0 +1,7 @@
+# Problem number 2: personal greeting
+puts "Please tell me your name"
+first = gets.chomp() # saves imput from user as a variable
+puts "And may I ask, what is your last name?"
+last = gets.chomp() # saves imput from user as a variable
+full = first + ' ' + last # combines both name variables with a space
+puts "Hello #{full}, nice to meet you." # prints greeting with name using interpolation 

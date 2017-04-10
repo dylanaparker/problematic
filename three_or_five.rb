a5cf909341af21de3f89a84cccf234b11a007818
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-10 15:38:54 -0700

    made an outline for solibing problem

---
+++ three_or_five.rb
@@ -0,0 +1,5 @@
+# problem number 5: add all numbers in range that are multiples of 3 and 5
+# populate range
+# loop through and separate if multiple of 3 or 5 to new array
+# loop through and add all numbers in new array
+# print the sum

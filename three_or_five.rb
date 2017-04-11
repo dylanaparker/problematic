d2be8a2872d59b9dcafe6c7555a95267eba7a9e2
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-10 22:17:50 -0700

    separated the multiples of 3 and 5

--- three_or_five.rb
+++ three_or_five.rb
@@ -1,5 +1,16 @@
 # problem number 5: add all numbers in range that are multiples of 3 and 5
+puts "Pick a number"
 # populate range
-# loop through and separate if multiple of 3 or 5 to new array
+last = gets.chomp.to_i
+range = (1..last).to_a
+sum = 0
+range_2 = []
+# separate if multiple of 3 or 5 to new array
+range.each do |number|
+  if number % 3 == 0 || number % 5 == 0
+    range_2.push(number)
+  end
+end
+puts range_2 # Used to test
 # loop through and add all numbers in new array
 # print the sum

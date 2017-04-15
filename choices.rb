2aa6e44af69d6e85307d5a9716e1df8cb9e64697
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-14 22:51:55 -0700

    outlined steps to solve problem

---
+++ choices.rb
@@ -0,0 +1,6 @@
+# problem number 6: choice to add or multiply all numbers in a range
+# populate range
+# choose to add or multiply store choice in variable
+# if add loop through and add
+# else if multiply loop through and multiply
+# print results

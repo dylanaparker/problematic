0b1a2c124fcaa16d09881e2e9de0d686007ab6b6
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-14 22:56:42 -0700

    completed the second step in the outline: decide to add or multiply

--- choices.rb
+++ choices.rb
@@ -4,6 +4,11 @@ last = gets.chomp.to_i
 numbers = (1..last).to_a
 puts numbers # test
 # choose to add or multiply store choice in variable
+puts "Would you like to add or multiply?\n
+To find the sum, type: ADD
+To find the product, type: MULTIPLY"
+choice = gets.chomp.upcase
+puts choice # test
 # if add loop through and add
 # else if multiply loop through and multiply
 # print results

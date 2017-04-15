775a5f6f5c6cea0d4fbea83713517c0169e71e90
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-14 22:54:02 -0700

    completed the first step in the outline: populated the range with numbers

--- choices.rb
+++ choices.rb
@@ -1,5 +1,8 @@
 # problem number 6: choice to add or multiply all numbers in a range
-# populate range
+# populate rangeputs 'Pick a number'
+last = gets.chomp.to_i
+numbers = (1..last).to_a
+puts numbers # test
 # choose to add or multiply store choice in variable
 # if add loop through and add
 # else if multiply loop through and multiply

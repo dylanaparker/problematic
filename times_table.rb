62ad85a217bf7a1961e5d7a1ce3332086d00e163
Dylan A. Parker <dylanaparkerdev@gmail.com>
2017-04-18 21:22:15 -0700

    outlined how to solve problem 7

---
+++ times_table.rb
@@ -0,0 +1,5 @@
+# problem number 7: print times table
+# get the number for the times tables
+# get the length for the times tables
+# do the math for the times tables
+# print the times table to the screen
